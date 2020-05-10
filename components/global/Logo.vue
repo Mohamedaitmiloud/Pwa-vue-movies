ff70d4feb6a8818e9fe006c9b44e4edb04a74c0c
Mohamed <mohammed.aitmiloud@gmail.com>
2020-05-10 18:53:22 +0000

    init commite

---
+++ components/global/Logo.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav :class="$style.nav">
+    <img src="~/assets/images/geekait.png" alt="logo" srcset="" style="width:10em; height:4em;">
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~/assets/css/utilities/_variables.scss';
+
+</style>
+
+<style lang="scss" module>
+@import '~/assets/css/utilities/_variables.scss';
+
+.nav {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 6rem;
+  padding: 0 5.5rem;
+  background-color:rgba(0, 0, 0, 0.7);
+
+}
+
+.text {
+  margin: 0;
+  overflow: hidden;
+  font-size: 3rem;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+</style>

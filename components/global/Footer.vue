68e221e60c44e5f70027c7db32460e70d0a73f33
Mohamed Ait Miloud <mohammed.aitmiloud@gmail.com>
2021-06-20 01:32:56 +0100

    Update Footer.vue

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,29 +1,7 @@
 <template>
   <footer :class="$style.footer">
     <p>&copy; {{ new Date().getFullYear() }} Geekait. All rights reserved.</p>
-    <!-- <p>Designed by <a target="_blank" href="#">A.Inajarin</a> and built by M.Aitmiloud, data provided by <a target="_blank" href="https://www.themoviedb.org/" rel="noopener">TMDb</a>.</p> -->
-
-    <ul class="nolist">
-      <li>
-        <a
-          href="https://www.linkedin.com/in/mohammed-aitmiloud/"
-          target="_blank"
-          aria-label="Link to LinkedIn account"
-          rel="noopener">
-          <!-- eslint-disable-next-line -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#585858"><path d="M19 0H5a5 5 0 0 0-5 5v14a5 5 0 0 0 5 5h14a5 5 0 0 0 5-5V5a5 5 0 0 0-5-5zM8 19H5V8h3v11zM6.5 6.732c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zM20 19h-3v-5.604c0-3.368-4-3.113-4 0V19h-3V8h3v1.765c1.396-2.586 7-2.777 7 2.476V19z"/></svg>
-        </a>
-      </li>
-      <li>
-        <a
-          href="mailto:mohammed.aitmiloud@gmail.com"
-          aria-label="Link to Email"
-          rel="noopener">
-          <!-- eslint-disable-next-line -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#585858"><path d="M0 3v18h24V3H0zm21.518 2L12 12.713 2.482 5h19.036zM2 19V7.183l10 8.104 10-8.104V19H2z"/></svg>
-        </a>
-      </li>
-    </ul>
+    <p>built by Mossaab el Hamzaoui, data provided by <a target="_blank" href="https://www.themoviedb.org/" rel="noopener">TMDb</a>.</p>
   </footer>
 </template>
 
